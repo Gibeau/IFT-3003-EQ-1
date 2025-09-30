96f2432865f806d68242c2a8b1b697d2ed6930ca
gabrielGibeau <gabriel.gibeau.1@ulaval.ca>
2025-09-29 22:40:38 -0400

    modifier l'architecture du programme pour ce conformer aux nouvelles exigences.

---
+++ src/main/java/ca/ulaval/glo2003/infra/persistence/InMemoryRestaurantRepository.java
@@ -0,0 +1,30 @@
+package ca.ulaval.glo2003.infra.persistence;
+
+import ca.ulaval.glo2003.entities.Restaurant;
+
+import java.util.*;
+
+public class InMemoryRestaurantRepository implements RestaurantRepository {
+    private final Map<String, Restaurant> database;
+
+    public InMemoryRestaurantRepository() {
+        database = new HashMap<>();
+    }
+
+    @Override
+    public void save(Restaurant restaurant) {
+        this.database.put(restaurant.getId(), restaurant);
+    }
+
+    @Override
+    public Optional<Restaurant> get(String id) {
+        return Optional.ofNullable(database.get(id));
+    }
+
+    @Override
+    public List<Restaurant> listParProprietaire(String proprietaireId) {
+        return database.values().stream()
+                .filter(r -> Objects.equals(r.getProprietaire().getId(), proprietaireId))
+                .toList();
+    }
+}
